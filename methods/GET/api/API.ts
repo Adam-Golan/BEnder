607be0c0ff725a923a41b2967d00797d7ccc69d8
ObscuraNull <obscuranull@gmail.com>
2024-05-19 08:17:00 +0300

    I do not know

--- methods/GET/api/API.ts
+++ methods/GET/api/API.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 export class GET_API extends RouterBase {
     protected setRouter(): void {
-        this.router.use(`/:action`, (req: Re quest, res: Response) => {
+        this.router.use(`/:action`, (req: Request, res: Response) => {
             let response, status;
             switch (req.params.action) {
                 default:
