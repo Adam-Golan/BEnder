29f367ad67ca727d279668b41658cfa4fb524a89
ObscuraNull <obscuranull@gmail.com>
2024-07-07 18:55:40 +0300

    Creating tryer function with error logging.

--- methods/base.ts
+++ methods/base.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { readFileSync, writeFileSync } from 'fs';
 
 export abstract class Synapse {
     router: Router = Router();
@@ -8,6 +9,18 @@ export abstract class Synapse {
     }
 
     protected abstract setRouter(): void;
+
+    protected async tryer<T>(fn: () => T, successCode: number = 200): Promise<{ code: number, data: T | unknown }> {
+        try {
+            return { code: successCode, data: await fn() };
+        } catch (err) {
+            const errors = await readFileSync('../logs/errors/errors.json', { encoding: 'utf8' });
+            const parsed: { timestamp: string, error: unknown }[] = JSON.parse(errors);
+            parsed.push({ timestamp: new Date().toLocaleString(), error: err });
+            await writeFileSync('../logs/errors/errors.json', JSON.stringify(parsed), { encoding: 'utf8' });
+            return { code: 500, data: err };
+        }
+    }
 }
 
 export abstract class Neuron extends Synapse {
