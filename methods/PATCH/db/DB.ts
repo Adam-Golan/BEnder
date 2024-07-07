6c1b4e95d0a4f1756b9cd36a3a28a20520d1b925
ObscuraNull <obscuranull@gmail.com>
2024-07-07 18:55:10 +0300

    Removing path bug.

--- methods/PATCH/db/DB.ts
+++ methods/PATCH/db/DB.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 export class PATCH_DB extends Synapse {
     protected setRouter(): void {
-        this.router.use(`/${this.baseRoute}/:action`, (req: Request, res: Response) => {
+        this.router.use(`/:action`, (req: Request, res: Response) => {
             let response, status;
             switch (req.params.action) {
                 default:
