79de6d87a9dbd081955ec9d39d3320d0d5018307
ObscuraNull <obscuranull@gmail.com>
2024-07-08 07:48:56 +0300

    Creating public directory string.

--- config/infrastructure.ts
+++ config/infrastructure.ts
@@ -4,9 +4,10 @@ import { join } from 'path';
 import cookieParser from 'cookie-parser';
 
 export const server: Express = express();
+export const publicDir = join(__dirname, '..', 'public');
 
 server
-    .use(express.static(join(__dirname, '..', 'public')))
+    .use(express.static(publicDir))
     .use(json())
     .use(cors())
     .use(cookieParser())
