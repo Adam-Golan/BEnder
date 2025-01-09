440e01d66c3256f5752eb43c5fc1f580051f7d49
Adam-Golan <slowmoon.adam@gmail.com>
2025-01-09 12:43:55 +0200

    Adding security to infrastructure, using appConfig.

--- config/infrastructure.ts
+++ config/infrastructure.ts
@@ -1,7 +1,11 @@
-import express, { Express, json } from "express";
+import express, { type Express, json } from "express";
 import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
+
+import { appConfig } from '../app.config';
 
 export const server: Express = express();
 export const publicDir = join(__dirname, '..', 'public');
@@ -9,5 +13,44 @@ export const publicDir = join(__dirname, '..', 'public');
 server
     .use(express.static(publicDir))
     .use(json())
-    .use(cors())
     .use(cookieParser())
+
+if (appConfig.security.helmet) {
+    server.use(helmet({
+        contentSecurityPolicy: appConfig.security.helmet.contentSecurityPolicy
+    }));
+}
+
+if (appConfig.security.cors) {
+    server.use(cors({
+        origin: appConfig.security.cors.origins ?? [],
+        methods: appConfig.security.cors.methods ?? [],
+        allowedHeaders: appConfig.security.cors.headers ?? [],
+    }));
+}
+
+if (appConfig.security.rateLimit) {
+    server.use(rateLimit({
+        windowMs: appConfig.security.rateLimit.windowMs ?? 15 * 60 * 1000,
+        max: appConfig.security.rateLimit.max ?? 100,
+    }));
+}
+
+if (appConfig.db) {
+    switch (appConfig.db.type) {
+        case 'cloud':
+            break;
+        case 'document':
+            break;
+        case 'graph':
+            break;
+        case 'keyvalue':
+            break;
+        case 'nosql':
+            break;
+        case 'sql':
+            break;
+        case 'timeseries':
+            break;
+    }
+}
