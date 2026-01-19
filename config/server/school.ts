b14d431ca233e0a6f3c389555b3f72e4c470703b
Adam-Golan <slowmoon.adam@gmail.com>
2026-01-19 10:40:58 +0200

    The birth of UniversalAdapter: First Steps.

--- config/server/school.ts
+++ config/server/school.ts
@@ -13,138 +13,162 @@ import {
     HttpMethod
 } from './types';
 
-export class UniversalAdapter {
-    [method: string]: ((path: string, ...handlers: IHandler[]) => void) | Promise<any> | null;
-    reqDigest: (req: any) => Promise<IFrameworkRequest> = async (req: IFrameworkRequest) => req;
-    resDigest: (res: any) => IFrameworkResponse = (res: IFrameworkResponse) => res;
-    use: (path: string | IHandler, ...handlers: IHandler[]) => void = () => null;
-    injectRouter: (path: string, router: any) => void = () => null;
-    createRouter: (path?: string) => any = () => null;
-    private routerModule: Promise<any> | null = null;
+// Extend IHandler for internal use to allow async returns
+type AsyncHeader = (req: IFrameworkRequest, res: IFrameworkResponse, next?: () => Promise<void>) => void | Promise<void>;
 
-    constructor(app: any, framework: NodeFrameworkType | BunFrameworkType) {
+export class UniversalAdapter {
+    [method: string]: ((path: string, ...handlers: IHandler[]) => void) | Promise<any> | any;
+
+    public reqDigest: (req: any) => Promise<IFrameworkRequest> = async (req: IFrameworkRequest) => req;
+    public resDigest: (res: any) => IFrameworkResponse = (res: IFrameworkResponse) => res;
+    public use: (path: string | IHandler, ...handlers: IHandler[]) => void = () => null;
+    public injectRouter: (path: string, router: any) => void = () => null;
+    public createRouter: (path?: string) => Promise<UniversalAdapter> = async () => this;
+
+    // Core references
+    private routerModule: Promise<any> | null = null;
+    private routingMethods: HttpMethod[] = ['get', 'post', 'put', 'delete', 'patch', 'head', 'options'];
+
+    constructor(public app: any, private framework: NodeFrameworkType | BunFrameworkType) {
         if (!app || !framework) throw new Error('App and Framework must be defined');
         if (!["express", "fastify", "koa", "hono", "elysia"].find((f) => f === framework)) throw new Error('Framework is not supported');
+        this.initializeAdapters();
+        this.bindMethods();
+    }
 
-        const routingMethods: HttpMethod[] = ['use', 'get', 'post', 'put', 'delete', 'patch', 'head', 'options'];
-        let handleHandler = (handler: IHandler): ((...args: any[]) => void) => async (req: IFrameworkRequest, res: any, next?: () => Promise<any>) => handler(await this.reqDigest(req), this.resDigest(res), next);
+    private initializeAdapters() {
+        let handleHandler = (handler: IHandler | AsyncHeader): ((...args: any[]) => void) => async (req: IFrameworkRequest, res: any, next?: () => Promise<any>) => {
+            return handler(await this.reqDigest(req), this.resDigest(res), next);
+        };
 
-        if (framework === 'express') {
+        if (this.framework === 'express') {
             this.resDigest = (res: IExpressResponse) => res;
-            this.use = (path, ...handlers) => handlers.forEach(handler => app.use(path, handleHandler(handler)));
+            this.use = (path, ...handlers) => handlers.forEach(handler => this.app.use(path, handleHandler(handler)));
             this.routerModule = import('express').then(({ Router }) => Router);
-            this.injectRouter = (path, router) => app.use(path, router);
-            this.createRouter = async () => (await this.routerModule)();
+            this.injectRouter = (path, router) => this.app.use(path, router.native);
+            this.createRouter = async () => {
+                const Router = await this.routerModule;
+                return new UniversalAdapter(Router(), this.framework);
+            };
         }
-        if (framework === 'fastify') {
+
+        if (this.framework === 'fastify') {
             this.resDigest = (res: IFastifyResponse) => {
                 const response: IFrameworkResponse = {
-                    set(key, value) {
-                        res.header(key, value);
-                        return response;
-                    },
-                    cookie(key, value) {
-                        res.cookie(key, value);
-                        return response;
-                    },
-                    status(code) {
-                        res.code(code);
-                        return response;
-                    },
+                    set: (key, value) => { res.header(key, value); return response; },
+                    cookie: (key, value) => { res.cookie(key, value); return response; },
+                    status: (code) => { res.code(code); return response; },
                     json: (data) => res.send(data),
                     redirect: (url) => res.redirect(url),
                 }
                 return response;
             };
-            handleHandler = (handler: IHandler) => async (req: IFrameworkRequest, res: IFastifyResponse) => handler(await this.reqDigest(req), this.resDigest(res));
-            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => app.addHook('onRequest', handleHandler(handler)));
+            handleHandler = (handler: IHandler | AsyncHeader) => async (req: IFrameworkRequest, res: IFastifyResponse) => {
+                return handler(await this.reqDigest(req), this.resDigest(res));
+            };
+            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => this.app.addHook('onRequest', handleHandler(handler)));
             this.routerModule = import('fastify').then(({ fastify }) => fastify);
-            this.injectRouter = (path, router) => app.register(router, { prefix: path });
-            this.createRouter = async () => (await this.routerModule)();
+            this.injectRouter = (path, router) => this.app.register(router.native, { prefix: path });
+            this.createRouter = async () => {
+                const fastify = await this.routerModule;
+                return new UniversalAdapter(fastify(), this.framework);
+            };
         }
-        if (framework === 'koa') {
+
+        if (this.framework === 'koa') {
             this.resDigest = (res: IKoaResponse) => {
                 const response: IFrameworkResponse = {
-                    cookie(key, value) {
-                        res.cookies.set(key, value);
-                        return response;
-                    },
-                    set(key, value) {
-                        res.set(key, value);
-                        return response;
-                    },
-                    status(code) {
-                        res.status = code;
-                        return response;
-                    },
-                    json: (data) => res.body = JSON.stringify(data),
+                    cookie: (key, value) => { res.cookies.set(key, value); return response; },
+                    set: (key, value) => { res.set(key, value); return response; },
+                    status: (code) => { res.status = code; return response; },
+                    json: (data) => res.body = data, // Auto-JSON stringify by Koa
                     redirect: (url) => res.redirect(url),
                 }
                 return response;
             };
-            handleHandler = (handler: IHandler) => async (ctx: { request: IFrameworkRequest, response: IKoaResponse }, next: () => Promise<any>) => handler(await this.reqDigest(ctx.request), await this.resDigest(ctx.response), next);
-            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => app.use(handleHandler(handler)));
+            handleHandler = (handler: IHandler | AsyncHeader) => async (ctx: { request: IFrameworkRequest, response: IKoaResponse }, next: () => Promise<any>) => {
+                return handler(await this.reqDigest(ctx.request), this.resDigest(ctx.response), next);
+            };
+            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => this.app.use(handleHandler(handler)));
             this.routerModule = import('@koa/router').then(({ Router }) => Router);
-            this.injectRouter = (_, router) => app.use(router.routes());
-            this.createRouter = async (path) => new (await this.routerModule)({ prefix: path });
+            this.injectRouter = (_, router) => this.app.use(router.native.routes());
+            this.createRouter = async (path) => {
+                const Router = await this.routerModule;
+                return new UniversalAdapter(new Router({ prefix: path }), this.framework);
+            };
         }
-        if (framework === 'hono') {
-            this.reqDigest = async (req: IHonoRequest) => ({ headers: req.headers(), query: req.query(), params: req.params(), body: await req.parseBody() });
+
+        if (this.framework === 'hono') {
+            this.reqDigest = async (req: IHonoRequest) => ({ headers: req.headers(), query: req.query(), params: req.params(), body: await req.parseBody().catch(() => ({})) });
             this.resDigest = (res: IHonoResponse) => {
                 const response: IFrameworkResponse = {
                     cookie: (key, value) => {
                         import('hono/cookie').then(({ setCookie }) => setCookie(res as any, key, value));
-                        return this.resDigest(res);
-                    },
-                    set(key, value) {
-                        res.header(key, value);
                         return response;
                     },
-                    status(code) {
-                        res.status(code);
-                        return response;
-                    },
+                    set: (key, value) => { res.header(key, value); return response; },
+                    status: (code) => { res.status(code); return response; },
                     json: (data) => res.json(data),
                     redirect: (url) => res.redirect(url),
                 }
                 return response;
             };
-            handleHandler = (handler: IHandler) => async (c: { req: IHonoRequest, res: IHonoResponse }) => handler(await this.reqDigest(c.req), await this.resDigest(c.res));
+            handleHandler = (handler: IHandler | AsyncHeader) => async (c: { req: IHonoRequest, res: IHonoResponse }) => {
+                return handler(await this.reqDigest(c.req), this.resDigest(c.res));
+            };
             this.use = (path, ...handlers) => {
                 typeof path === 'string'
-                    ? handlers.forEach(handler => app.use(path, handleHandler(handler)))
-                    : [path, ...handlers].forEach(handler => app.use("*", handleHandler(handler)));
+                    ? handlers.forEach(handler => this.app.use(path, handleHandler(handler)))
+                    : [path, ...handlers].forEach(handler => this.app.use("*", handleHandler(handler)));
             };
             this.routerModule = import('hono').then(({ Hono }) => Hono);
-            this.injectRouter = (path, router) => app.route(path, router);
-            this.createRouter = async () => new (await this.routerModule)();
+            this.injectRouter = (path, router) => this.app.route(path, router.native);
+            this.createRouter = async () => {
+                const Hono = await this.routerModule;
+                return new UniversalAdapter(new Hono(), this.framework);
+            }
         }
-        if (framework === 'elysia') {
+
+        if (this.framework === 'elysia') {
             this.resDigest = (res: IElysiaResponse) => {
                 const response: IFrameworkResponse = {
-                    cookie(key, value) {
-                        res.cookie[key] = import('elysia').then(({ Cookie }) => new Cookie(key, {}, { value })) as any;
+                    cookie: (key, value) => {
+                        // @ts-ignore
+                        res.cookie[key].value = value;
                         return response;
                     },
-                    set(key, value) {
-                        res.headers[key] = value;
-                        return response;
-                    },
-                    status(code) {
-                        res.status(code);
-                        return response;
-                    },
-                    json: (data) => res.body = JSON.stringify(data),
+                    set: (key, value) => { res.headers[key] = value; return response; },
+                    status: (code) => { res.status(code); return response; },
+                    json: (data) => { res.body = data; }, // Elysia handles serialization
                     redirect: (url) => res.redirect(url),
                 }
                 return response;
             };
-            handleHandler = (handler: IHandler) => async (ctx: IFrameworkRequest & IElysiaResponse) => handler(await this.reqDigest(ctx), await this.resDigest(ctx));
-            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => app.onBeforeHandle(handleHandler(handler)));
+            handleHandler = (handler: IHandler | AsyncHeader) => async (ctx: IFrameworkRequest & IElysiaResponse) => {
+                return handler(await this.reqDigest(ctx), this.resDigest(ctx));
+            };
+            this.use = (path, ...handlers) => (typeof path === 'string' ? handlers : [path, ...handlers]).forEach(handler => this.app.onBeforeHandle(handleHandler(handler)));
             this.routerModule = import('elysia').then(({ Elysia }) => Elysia);
-            this.injectRouter = (_, router) => app.use(router);
-            this.createRouter = async (path) => new (await this.routerModule)({ prefix: path });
+            this.injectRouter = (_, router) => this.app.use(router.native);
+            this.createRouter = async (path) => {
+                const Elysia = await this.routerModule;
+                return new UniversalAdapter(new Elysia({ prefix: path }), this.framework);
+            }
         }
-        setTimeout(() => routingMethods.forEach(method => this[method] = (path, ...handlers) => handlers.forEach(handler => app[method](path, handleHandler(handler)))));
+
+        // Bind HTTP methods
+        for (const method of this.routingMethods)
+            this[method] = (path: string, ...handlers: IHandler[]) => {
+                for (const handler of handlers) this.app[method](path, handleHandler(handler));
+            }
+    }
+
+    private bindMethods() {
+        // Ensure all methods are bound to this instance
+        this.use = this.use.bind(this);
+        this.injectRouter = this.injectRouter.bind(this);
+        this.createRouter = this.createRouter.bind(this);
+        for (const method of this.routingMethods)
+            if (typeof this[method] === 'function') this[method] = (this[method] as Function).bind(this);
     }
 }
